08edf6b0f2e47b05b04cc177d3dbb92c51c56645
Zhenchen Xiao <t-zhxia@microsoft.com>
2018-07-10 16:49:45 +0800

    Revert "test build"
    This reverts commit 6b4c584784deb2b301c7e42bf62fcb04478b1c28.

--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-    description = "The location/region where the database and server are created. Changing this forces a new resource to be created."
+  description = "The location/region where the database and server are created. Changing this forces a new resource to be created."
 }
 
 variable "server_version" {
-description = "The version for the database server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
-  default = "12.0"
+  description = "The version for the database server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
+  default     = "12.0"
 }
 
 variable "db_name" {
