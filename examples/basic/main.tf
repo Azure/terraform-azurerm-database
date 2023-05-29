bba3a6f0021874f71140cbbe49acd4ad9d6e4b08
lonegunmanb <lonegunmanb@hotmail.com>
2023-05-29 16:09:50 +0800

    add azurerm_sql_active_directory_administratorresource (#26)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -6,6 +6,8 @@ resource "random_password" "password" {
   length = 20
 }
 
+data "azurerm_client_config" "current" {}
+
 module "sql_database" {
   source              = "../.."
   resource_group_name = "${var.resource_group_name}-${random_id.name.hex}"
@@ -13,6 +15,11 @@ module "sql_database" {
   db_name             = "${var.db_name}-${random_id.name.hex}"
   sql_admin_username  = var.sql_admin_username
   sql_password        = random_password.password.result
-  start_ip_address    = var.start_ip_address
-  end_ip_address      = var.end_ip_address
+  sql_aad_administrator = {
+    login     = "sqladmin"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+  }
+  start_ip_address = var.start_ip_address
+  end_ip_address   = var.end_ip_address
 }
